2a7fd58e57b8692d4b7ea5a8f5c30edd4e5940db
Kong Her <herk4@students.westerntc.edu>
2025-02-12 20:11:36 -0600

    Updated markdown-helper-tests.js to log markdown meta data in console

--- samples/markdown-helper-tests.js
+++ samples/markdown-helper-tests.js
@@ -1,4 +1,6 @@
-const {convertMarkdown, getMarkdownMetaData} = require("../modules/markdown-helpers");
+const { convertMarkdown, getMarkdownMetaData } = require("../modules/markdown-helpers");
 
-const filePath = __dirname + "/sample.md"
+const filePath = __dirname + "/sample.md";
 console.log(convertMarkdown(filePath));
+
+console.log(getMarkdownMetaData(filePath));
